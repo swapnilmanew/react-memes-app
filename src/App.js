b26b7f445a75baaadf3465416553a82e65e9279c
swapnilmajspire <swapnilm.ajspire@gmail.com>
2022-04-02 21:18:43 +0530

    re-upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  LinearProgress,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [image, setImage] = useState("");
+  const getMeme = async () => {
+    setLoading(true);
+    const response = await axios.get("https://meme-api.herokuapp.com/gimme");
+    setImage(response.data.preview[2]);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMeme();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Grid container style={{ marginTop: 20 }}>
+        <Grid item style={{ margin: "auto" }}>
+          <Card>
+            {loading == true ? <LinearProgress /> : ""}
+            <CardContent style={{ textAlign: "center" }}>
+              <Typography variant="h5">Memes App</Typography>
+              {image && image != null ? (
+                <CardMedia
+                  component="img"
+                  height="300"
+                  width="90%"
+                  image={image}
+                ></CardMedia>
+              ) : (
+                <div style={{ margin: "10px 5px" }}>
+                  Meme nahi sapadli re...Button vr click kr...!!
+                </div>
+              )}
+              <Button
+                variant="outlined"
+                style={{ marginTop: 20 }}
+                onClick={() => getMeme()}
+              >
+                Get A New Meme
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
